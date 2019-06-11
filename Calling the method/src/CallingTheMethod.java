1769099812c04f91b97cede972a2e8148cd3bbf6
kodMaker <radek4001@wp.pl>
2019-06-11 16:02:15 +0200

    Create CallingTheMethod.java

---
+++ Calling the method/src/CallingTheMethod.java
@@ -0,0 +1,22 @@
+public class CallingTheMethod {
+
+    public static void main(String[] args) {
+        ifTemperatureIsPositive(23);
+
+    }
+
+    public static boolean temperature(int someTemperature) {
+        return someTemperature > 0;
+    }
+
+
+    public static void ifTemperatureIsPositive(int someTemperature) {
+
+        boolean comingTemperature = temperature(someTemperature);
+
+        if (comingTemperature)
+            System.out.println("Temperature is positive");
+        else
+            System.out.println("Temperature is negative or zero");
+    }
+}
