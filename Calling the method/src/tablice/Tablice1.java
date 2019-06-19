6b58d15a7293930a11ff50877c675d4d716d6b6e
kodMaker <radek4001@wp.pl>
2019-06-19 14:02:05 +0200

    Update Tablice1.java

--- Calling the method/src/tablice/Tablice1.java
+++ Calling the method/src/tablice/Tablice1.java
@@ -5,12 +5,32 @@ public class Tablice1 {
     public static void main(String[] args) {
 
         Tablice1 main1 = new Tablice1();
-        int[] numbers = {3, 8};
-        System.out.println(main1.suma(numbers));
+        int[] numbers1 = {3, 8};
+        System.out.println(main1.suma(numbers1));
+
+        int[] numbers2 = {200, 320, 23};
+        System.out.println(theBiggestNumber(numbers2));
+        System.out.println(theBiggestNumberWithoutIf(numbers2));
     }
 
 
     public int suma(int[] numberss) {
         return numberss[0] + numberss[1];
+    }
+
+    public static int theBiggestNumber(int[] numbers2) {
+        int biggest = numbers2[0];
+
+        if(numbers2[1] > biggest)
+            biggest = numbers2[1];
+
+        if (numbers2[2] > biggest)
+            biggest = numbers2[2];
+
+        return biggest;
+    }
+
+    public static int theBiggestNumberWithoutIf (int[] numbers2) {
+        return Math.max(numbers2[0], Math.max(numbers2[1], numbers2[2]));
     }
 }
