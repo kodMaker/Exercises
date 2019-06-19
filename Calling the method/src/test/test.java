41968870e5a35846237d7ae35da165e2040a8bc6
kodMaker <radek4001@wp.pl>
2019-06-19 13:44:42 +0200

    Create test.java

---
+++ Calling the method/src/test/test.java
@@ -0,0 +1,16 @@
+package test;
+
+public class test {
+    private int abc = 5;
+
+    public int getAbc() {
+        return abc;
+    }
+
+    public void setAbc(int abc) {
+        this.abc = abc;
+    }
+    public static void method() {
+
+    }
+}
